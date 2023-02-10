a2316f9339b64ed4cfbbcf17f4154a07a4faf925
Ibaloc <Ibaloc@DESKTOP-26ROKQ6>
2023-02-10 14:38:53 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+            Console.Write("Enter an Integer: ");
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(isPrimeOrEven(num));
+            }
+            catch { Console.WriteLine("Not Odd/Even!"); }
+        }
+
+        static string isPrimeOrEven (int? num)
+        {
+            if (num % 2 == 0)
+            {
+                return "Even";
+            } 
+            else if(num % 3 == 0)
+            {
+                return "Odd";
+            }
+            else
+                return "Not Even/Odd";
+
+        }
+    }
+}
